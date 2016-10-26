eace9e4041fdecb5d243bf9acb235e67e3ab291a
Michal <michal@MacBook-Michal.local>
2016-10-26 11:36:34 +0200

    connection to API made

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Init from './components/Init';
+import axios from 'axios';
+
+export default class App extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://swapi.co/api/people').then((results) => {
+            this.setState({
+                users: results.data.results
+            })
+        })
+    }
+
+    render(){
+
+        return(
+            <div>
+                <h1> Star Wars Characters:</h1>
+                {this.state.users.map((user) => {
+                    return <User name={user.name} key={user.name} />
+                })}                
+            </div> 
+        );
+    }
+}
+
+let User = (props) => {
+    return(
+        <div className="user">
+            <div>Name: {props.name}</div>
+        </div>
+    )     
+}
 
 
-
-
-ReactDOM.render(<Init />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
